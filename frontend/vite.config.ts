b2307fbeaa717bea89576a873a5b4dc68da2e82c
lsh34206 <lsh34206@gmail.com>
2026-08-26 13:05:31 +0900

    1.2
    BUILDED BETA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    allowedHosts:["2359-124-194-149-252.ngrok-free.app",]
-  }
+  plugins: [react()]
 })
